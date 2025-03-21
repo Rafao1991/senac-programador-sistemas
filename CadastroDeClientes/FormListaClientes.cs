3deb6b3f75a982a0e021a9adb6739835eaf158ee
Rafael Sousa <rcsva_sousa@hotmail.com>
2025-03-21 15:48:36 -0300

    ---

--- CadastroDeClientes/FormListaClientes.cs
+++ CadastroDeClientes/FormListaClientes.cs
@@ -8,16 +8,286 @@ namespace CadastroDeClientes
         public FormListaClientes()
         {
             InitializeComponent();
+        }
 
-            Endereco enderecoRafael = new() { Logradouro = "Endereco do Rafael", Numero = "10", Bairro = "Bairro", CEP = "00000-001" };
-            Cliente rafael = new() { Id = 1, Nome = "Rafael Sousa", DataNascimento = "31/08/1991", Etnia = Etnia.OUTROS, Tipo = TipoCliente.PF, Endereco = enderecoRafael };
+        private bool NomeInvalido()
+        {
+            if (string.IsNullOrWhiteSpace(textBoxNome.Text))
+            {
+                labelErro.Text = "O campo Nome é obrigatório";
+                textBoxNome.Focus();
+                return true;
+            }
+
+            if (!textBoxNome.Text.Contains(' '))
+            {
+                labelErro.Text = "O campo Nome deve conter nome e sobrenome";
+                textBoxNome.Focus();
+                return true;
+            }
+
+            labelErro.Text = "";
+            return false;
+        }
+
+        private bool DataNascimentoInvalida()
+        {
+            if (string.IsNullOrWhiteSpace(maskedTextBoxDataNascimento.Text) || maskedTextBoxDataNascimento.Text == "  /  /" || maskedTextBoxDataNascimento.Text.Length < 10)
+            {
+                labelErro.Text = "O campo Data de Nascimento é obrigatório";
+                maskedTextBoxDataNascimento.Focus();
+                return true;
+            }
+
+            if (!DateTime.TryParse(maskedTextBoxDataNascimento.Text, out DateTime dataNascimento))
+            {
+                labelErro.Text = "O campo Data de Nascimento deve ser uma data válida";
+                maskedTextBoxDataNascimento.Focus();
+                return true;
+            }
+
+            if (dataNascimento > DateTime.Today)
+            {
+                labelErro.Text = "O campo Data de Nascimento deve ser uma data no passado";
+                maskedTextBoxDataNascimento.Focus();
+                return true;
+            }
+
+            if (DateTime.Today.Year - dataNascimento.Year < 18)
+            {
+                labelErro.Text = "O campo Data de Nascimento deve ser de um cliente maior de idade";
+                maskedTextBoxDataNascimento.Focus();
+                return true;
+            }
+
+            if (DateTime.Today.Year - dataNascimento.Year > 120)
+            {
+                labelErro.Text = "O campo Data de Nascimento deve ser de um cliente menor de 120 anos";
+                maskedTextBoxDataNascimento.Focus();
+                return true;
+            }
+
+            labelErro.Text = "";
+            return false;
+        }
+
+        private bool EmailInvalido()
+        {
+            if (string.IsNullOrWhiteSpace(textBoxEmail.Text))
+            {
+                labelErro.Text = "O campo Email é obrigatório";
+                textBoxEmail.Focus();
+                return true;
+            }
+
+            if (!textBoxEmail.Text.Contains('@') && !textBoxEmail.Text.Contains('.'))
+            {
+                labelErro.Text = "O campo Email deve ser um email válido";
+                textBoxEmail.Focus();
+                return true;
+            }
+
+            foreach (Cliente cliente in Clientes)
+            {
+                if (cliente.Email == textBoxEmail.Text)
+                {
+                    labelErro.Text = "O campo Email deve ser único";
+                    textBoxEmail.Focus();
+                    return true;
+                }
+            }
+
+            labelErro.Text = "";
+            return false;
+        }
+
+        private bool TelefoneInvalido()
+        {
+            if (string.IsNullOrWhiteSpace(maskedTextBoxTelefone.Text) || maskedTextBoxTelefone.Text == "(  )      -" || maskedTextBoxTelefone.Text.Length < 15)
+            {
+                labelErro.Text = "O campo Telefone é obrigatório";
+                maskedTextBoxTelefone.Focus();
+                return true;
+            }
+
+            foreach (Cliente cliente in Clientes)
+            {
+                if (cliente.Telefone == maskedTextBoxTelefone.Text)
+                {
+                    labelErro.Text = "O campo Telefone deve ser único";
+                    maskedTextBoxTelefone.Focus();
+                    return true;
+                }
+            }
+
+            labelErro.Text = "";
+            return false;
+        }
+
+        private bool TipoClienteInvalido()
+        {
+            if (!radioButtonPf.Checked && !radioButtonPj.Checked)
+            {
+                labelErro.Text = "O campo Tipo de Cliente é obrigatório";
+                return true;
+            }
+            labelErro.Text = "";
+            return false;
+        }
+
+        private bool EtniaInvalida()
+        {
+            if (comboBoxEtnia.SelectedIndex == -1)
+            {
+                labelErro.Text = "O campo Etnia é obrigatório";
+                comboBoxEtnia.Focus();
+                return true;
+            }
+            labelErro.Text = "";
+            return false;
+        }
+
+        private bool LogradouroInvalido()
+        {
+            if (string.IsNullOrWhiteSpace(textBoxLogradouro.Text))
+            {
+                labelErro.Text = "O campo Logradouro é obrigatório";
+                textBoxLogradouro.Focus();
+                return true;
+            }
+            labelErro.Text = "";
+            return false;
+        }
+
+        private bool NumeroInvalido()
+        {
+            if (string.IsNullOrWhiteSpace(textBoxNumero.Text))
+            {
+                labelErro.Text = "O campo Número é obrigatório";
+                textBoxNumero.Focus();
+                return true;
+            }
+            labelErro.Text = "";
+            return false;
+        }
+
+        private bool BairroInvalido()
+        {
+            if (string.IsNullOrWhiteSpace(textBoxBairro.Text))
+            {
+                labelErro.Text = "O campo Bairro é obrigatório";
+                textBoxBairro.Focus();
+                return true;
+            }
+            labelErro.Text = "";
+            return false;
+        }
+
+        private bool CEPInvalido()
+        {
+            if (string.IsNullOrWhiteSpace(maskedTextBoxCEP.Text) || maskedTextBoxCEP.Text == "     -" || maskedTextBoxCEP.Text.Length < 9)
+            {
+                labelErro.Text = "O campo CEP é obrigatório";
+                maskedTextBoxCEP.Focus();
+                return true;
+            }
+            labelErro.Text = "";
+            return false;
+        }
+
+        private int GerarId()
+        {
+            if (Clientes.Count == 0)
+            {
+                return 1;
+            }
+
+            int novoId = 0;
+            foreach (Cliente cliente in Clientes)
+            {
+                if (cliente.Id > novoId)
+                {
+                    novoId = cliente.Id;
+                }
+            }
+
+            return novoId + 1;
+        }
+
+        private void LimparForm()
+        {
+            textBoxNome.Clear();
+            textBoxNomeSocial.Clear();
+            maskedTextBoxDataNascimento.Clear();
+            textBoxEmail.Clear();
+            maskedTextBoxTelefone.Clear();
+            comboBoxEtnia.SelectedIndex = -1;
+            radioButtonPf.Checked = false;
+            radioButtonPj.Checked = false;
+
+            textBoxLogradouro.Clear();
+            textBoxNumero.Clear();
+            textBoxBairro.Clear();
+            maskedTextBoxCEP.Clear();
+        }
+
+        private void FormListaClientes_Load(object sender, EventArgs e)
+        {
+            labelErro.Text = "";
+
+            Enum.GetNames(typeof(Etnia)).ToList().ForEach(etnia => comboBoxEtnia.Items.Add(etnia));
+
+            Endereco enderecoRafael = new()
+            {
+                Logradouro = "Endereco do Rafael",
+                Numero = "10",
+                Bairro = "Bairro",
+                CEP = "00000-001"
+            };
+            Cliente rafael = new()
+            {
+                Id = GerarId(),
+                Nome = "Rafael Sousa",
+                DataNascimento = "31/08/1991",
+                Email = "rafael@email.com",
+                Telefone = "(11) 99999-9990",
+                Etnia = Etnia.OUTROS,
+                Tipo = TipoCliente.PF,
+                Endereco = enderecoRafael
+            };
             Clientes.Add(rafael);
 
-            Endereco enderecoVanda = new() { Logradouro = "Endereco dos pais do Rafael", Numero = "301", Bairro = "Bairro", CEP = "00000-001" };
-            Cliente vanda = new() { Id = 2, Nome = "Vanda Maria", DataNascimento = "08/03/1958", Etnia = Etnia.BRANCO, Tipo = TipoCliente.PF, Endereco = enderecoVanda };
+            Endereco enderecoVanda = new()
+            {
+                Logradouro = "Endereco dos pais do Rafael",
+                Numero = "301",
+                Bairro = "Bairro",
+                CEP = "00000-001"
+            };
+            Cliente vanda = new()
+            {
+                Id = GerarId(),
+                Nome = "Vanda Maria",
+                DataNascimento = "08/03/1958",
+                Email = "vanda@email.com",
+                Telefone = "(11) 99999-9991",
+                Etnia = Etnia.BRANCO,
+                Tipo = TipoCliente.PF,
+                Endereco = enderecoVanda
+            };
             Clientes.Add(vanda);
 
-            Cliente joao = new() { Id = 3, Nome = "Joao Sousa", DataNascimento = "14/07/1962", Etnia = Etnia.NEGRO, Tipo = TipoCliente.PF, Endereco = enderecoVanda };
+            Cliente joao = new()
+            {
+                Id = GerarId(),
+                Nome = "Joao Sousa",
+                DataNascimento = "14/07/1962",
+                Email = "joao@email.com",
+                Telefone = "(11) 99999-9992",
+                Etnia = Etnia.NEGRO,
+                Tipo = TipoCliente.PF,
+                Endereco = enderecoVanda
+            };
             Clientes.Add(joao);
 
 
@@ -25,34 +295,59 @@ namespace CadastroDeClientes
             dataGridViewClientes.DataSource = BindingSource;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void buttonAdicionar_Click(object sender, EventArgs e)
         {
-            Endereco enderecoGustavo = new() { Logradouro = "Endereco dos Gustavo", Numero = "852", Bairro = "Bairro", CEP = "00000-002" };
-            Cliente gustavo = new() { Id = 4, Nome = "Gustavo Sousa", DataNascimento = "09/11/1993", Etnia = Etnia.BRANCO, Tipo = TipoCliente.PF, Endereco = enderecoGustavo };
-            Clientes.Add(gustavo);
+            if (NomeInvalido() || DataNascimentoInvalida() || EmailInvalido() || TelefoneInvalido() || TipoClienteInvalido() || EtniaInvalida())
+            {
+                return;
+            }
+
+            if (LogradouroInvalido() || NumeroInvalido() || BairroInvalido() || CEPInvalido())
+            {
+                return;
+            }
+
+            Endereco novoEndereco = new()
+            {
+                Logradouro = textBoxLogradouro.Text,
+                Numero = textBoxNumero.Text,
+                Bairro = textBoxBairro.Text,
+                CEP = maskedTextBoxCEP.Text,
+            };
+
+            Cliente novoCliente = new()
+            {
+                Id = GerarId(),
+                Nome = textBoxNome.Text,
+                NomeSocial = textBoxNomeSocial.Text,
+                DataNascimento = maskedTextBoxDataNascimento.Text,
+                Email = textBoxEmail.Text,
+                Telefone = maskedTextBoxTelefone.Text,
+                Etnia = (Etnia)comboBoxEtnia.SelectedIndex,
+                Tipo = radioButtonPf.Checked ? TipoCliente.PF : TipoCliente.PJ,
+                Endereco = novoEndereco
+            };
+            Clientes.Add(novoCliente);
 
             BindingSource.ResetBindings(false);
+
+            LimparForm();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void buttonRemover_Click(object sender, EventArgs e)
         {
+            if (dataGridViewClientes.SelectedRows.Count == 0 || dataGridViewClientes.SelectedRows[0].Index < 0)
+            {
+                return;
+            }
+
             Clientes.RemoveAt(dataGridViewClientes.SelectedRows[0].Index);
             BindingSource.ResetBindings(false);
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void label3_Click(object sender, EventArgs e)
         {
-            FormDetalheCliente formDetalheCliente;
-            if (dataGridViewClientes.SelectedRows.Count == 0 || dataGridViewClientes.SelectedRows[0].Index < 0)
-            {
-                formDetalheCliente = new();
-            }
-            else
-            {
-                formDetalheCliente = new(Clientes.ElementAt(dataGridViewClientes.SelectedRows[0].Index));
-            }
 
-            formDetalheCliente.ShowDialog();
         }
     }
 }
