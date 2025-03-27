0caa4069ab3d523e398dcbf9df12f04bff7a60c1
RAFAEL DE CARVALHO E SILVA VOLPE ALONSO DE SOUSA <rafael.csvasousa@senacsp.edu.br>
2025-03-26 22:06:58 -0300

    bd aula 02

--- BancoDeDados/table-usuario.sql
+++ BancoDeDados/table-usuario.sql
@@ -4,36 +4,3 @@ CREATE TABLE IF NOT EXISTS usuario (
     email VARCHAR(50) NOT NULL UNIQUE,
     senha VARCHAR(16) NOT NULL
 );
-
-SELECT 
-    *
-FROM
-    usuario;
-
-INSERT INTO usuario (
-	nome,
-    email,
-    senha
-)
-VALUES (
-	'rafael sousa',
-    'rafael.csvasousa@email.com',
-    'Senha@123'
-);
-
-INSERT INTO usuario (nome, email, senha)
-VALUES 
-	('Neymar Jr.', 'njr@email.com', 'Brun@'),
-	('Maria Madalena', 'mmadalena@email.com', 'a1s2d3f4!'),
-	('Pablo Vitar', 'p.vitar@email.com', 'Senh@Fort3');
-
-INSERT INTO usuario (
-	nome,
-    email,
-    senha
-)
-VALUES (
-	'João Silva',
-    'silva.joao@email.com',
-    'Senh@123'
-);
