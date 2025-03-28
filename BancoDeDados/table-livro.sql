01676c701c0bd6c0b7bda72881c58a5c1ee010be
RAFAEL DE CARVALHO E SILVA VOLPE ALONSO DE SOUSA <rafael.csvasousa@senacsp.edu.br>
2025-03-27 22:05:46 -0300

    bd aula 03

--- BancoDeDados/table-livro.sql
+++ BancoDeDados/table-livro.sql
@@ -5,44 +5,7 @@ CREATE TABLE IF NOT EXISTS livro (
     numero_paginas INT NULL,
     preco DECIMAL(10, 2) NULL,
     isbn VARCHAR(17) NOT NULL UNIQUE,
-    genero VARCHAR(30) NOT NULL,
-    editora VARCHAR(50) NOT NULL,
-    autor VARCHAR(100) NOT NULL
-);
-
-INSERT INTO livro (
-	titulo, data_publicacao, preco, isbn, genero, editora, autor
-) VALUES (
-	'titulo do meu livro',
-    '1991-08-31 22:38:48',
-    129.99,
-    '978-3-16-148410-0',
-    'tecnologia',
-    'editora',
-    'rafael sousa'
-);
-
-INSERT INTO livro (
-	titulo, data_publicacao, preco, isbn, genero, editora, autor
-) VALUES (
-	'titulo do meu livro 2',
-    '1994-08-31',
-    87.99,
-    '978-3-16-148410-1',
-    'tecnologia',
-    'editora',
-    'rafael sousa'
-);
-
-INSERT INTO livro (
-	titulo, data_publicacao, numero_paginas, preco, isbn, genero, editora, autor
-) VALUES (
-	'titulo do meu livro 3',
-    '1998-08-31',
-    209,
-    93.99,
-    '978-3-16-148410-2',
-    'tecnologia',
-    'editora',
-    'rafael sousa'
+    id_genero INT NOT NULL,
+    id_editora INT NOT NULL,
+    id_autor INT NOT NULL
 );
