c1ac21372db20b35f0f1a45decfb96df11dc13d7
RAFAEL DE CARVALHO E SILVA VOLPE ALONSO DE SOUSA <rafael.csvasousa@senacsp.edu.br>
2025-03-28 22:12:06 -0300

    bd aula 04

--- BancoDeDados/select-livro-genero-editora-autor.sql
+++ BancoDeDados/select-livro-genero-editora-autor.sql
@@ -1,4 +1,41 @@
 SELECT 
+    l.id,
+    l.titulo,
+    l.data_publicacao,
+    l.numero_paginas,
+    l.preco,
+    l.isbn,
+    g.nome AS genero,
+    e.nome AS editora,
+    a.nome AS autor,
+    l.criado_em
+FROM
+    livro l
+        INNER JOIN
+    genero g ON l.id_genero = g.id
+        INNER JOIN
+    editora e ON l.id_editora = e.id
+        INNER JOIN
+    autor a ON l.id_autor = a.id
+WHERE l.apagado = 0;
+
+UPDATE livro l 
+SET 
+    l.apagado = 1
+WHERE
+    l.id_autor = (SELECT 
+            a.id
+        FROM
+            autor a
+        WHERE
+            a.nome = 'rafael sousa');
+            
+DELETE FROM livro 
+WHERE
+    id_autor = 1;
+
+/*
+SELECT 
     livro.id,
     livro.titulo,
     livro.data_publicacao,
@@ -17,3 +54,4 @@ WHERE
     livro.id_genero = genero.id
         AND livro.id_editora = editora.id
         AND livro.id_autor = autor.id;
+*/
--- BancoDeDados/table-livro.sql
+++ BancoDeDados/table-livro.sql
@@ -3,9 +3,17 @@ CREATE TABLE IF NOT EXISTS livro (
     titulo VARCHAR(100) NOT NULL,
     data_publicacao DATETIME NOT NULL,
     numero_paginas INT NULL,
-    preco DECIMAL(10, 2) NULL,
+    preco DECIMAL(10 , 2 ) NULL,
     isbn VARCHAR(17) NOT NULL UNIQUE,
+    apagado BIT NOT NULL DEFAULT 0,
     id_genero INT NOT NULL,
     id_editora INT NOT NULL,
-    id_autor INT NOT NULL
+    id_autor INT NOT NULL,
+    criado_em DATETIME NOT NULL DEFAULT NOW(),
+    FOREIGN KEY (id_genero)
+        REFERENCES genero (id),
+    FOREIGN KEY (id_editora)
+        REFERENCES editora (id),
+    FOREIGN KEY (id_autor)
+        REFERENCES autor (id)
 );
